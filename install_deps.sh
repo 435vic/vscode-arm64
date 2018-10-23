bbcdf5558b63ac567721ec91c67f755b6afc9275
Victor Javier <victor_javier2004@hotmail.com>
2018-10-23 12:58:39 +0000

    Install yarn with repository npm didn't work. Using the repostory now. :/

--- install_deps.sh
+++ install_deps.sh
@@ -1,6 +1,11 @@
 sudo apt-get update
 sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm
+# Install yarn from the package repository,
+# since npm does not actually work
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
 # Update yarn
-npm update -g yarn
+# npm update -g yarn
+sudo apt-get insall yarn --upgrade
 echo "=========  YARN WAS UPDATED  ========="
 yarn --version
