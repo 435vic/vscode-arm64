b8f46daa1d52e54dc5791ab9dc5a9e3b518c1cda
Victor Javier <victor_javier2004@hotmail.com>
2018-10-23 12:45:36 +0000

    update yarn version

--- install_deps.sh
+++ install_deps.sh
@@ -1,2 +1,6 @@
 sudo apt-get update
 sudo apt-get install libx11-dev libxkbfile-dev libsecret-1-dev fakeroot rpm
+# Update yarn
+npm update -g yarn
+echo "=========  YARN WAS UPDATED  ========="
+yarn --version
