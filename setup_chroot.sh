75115ba4864da9783d0a7744b9a2386e98da96ff
Victor Javier <victor_javier2004@hotmail.com>
2018-10-23 17:51:53 +0000

    apt-get update before install of libx11-dev

--- setup_chroot.sh
+++ setup_chroot.sh
@@ -7,13 +7,13 @@ trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
 trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
 
 # Install dependencies needed for qemu
-sudo apt install qemu qemu-user-static debootstrap gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
+sudo apt-get install qemu qemu-user-static debootstrap gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 # Create rootfs/chroot for arm64
 echo "======== Setting up qemu rootfs... ========"
 sudo qemu-debootstrap --arch=arm64 --variant=minbase xenial rootfs
 echo "Done."
 echo "Installing libraries on chroot..."
-sudo chroot rootfs apt-get install -y libx11-dev
+sudo chroot rootfs apt-get update && apt-get install -y libx11-dev
 echo "Done."
 # Set up target toolchain for arm64
 export CC=/usr/bin/cc
