ab506683060d9f403431152daf53abecd1ed76a1
Victor Javier <victor_javier2004@hotmail.com>
2018-10-23 18:21:38 +0000

    remove libx11-dev

--- setup_chroot.sh
+++ setup_chroot.sh
@@ -12,9 +12,9 @@ sudo apt-get install qemu qemu-user-static debootstrap gcc-arm-linux-gnueabihf g
 echo "======== Setting up qemu rootfs... ========"
 sudo qemu-debootstrap --arch=arm64 --variant=minbase xenial rootfs
 echo "Done."
-echo "Installing libraries on chroot..."
-sudo chroot rootfs apt-get update 
-sudo chroot rootfs apt-get install -y libX11-dev
+#echo "Installing libraries on chroot..."
+#sudo chroot rootfs apt-get update 
+#sudo chroot rootfs apt-get install -y libX11-dev
 echo "Done."
 # Set up target toolchain for arm64
 export CC=/usr/bin/cc
