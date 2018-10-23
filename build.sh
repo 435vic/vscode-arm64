2afeac92dcf87390d37624034e8a9c1e7f301447
Victor Javier <victor_javier2004@hotmail.com>
2018-10-23 18:54:22 +0000

    remove tags

--- build.sh
+++ build.sh
@@ -1,11 +1,6 @@
 # exit when any command fails
 set -e
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 cd vscode
 
 export npm_config_arch=arm64
--- setup_chroot.sh
+++ setup_chroot.sh
@@ -1,11 +1,6 @@
 # exit when any command fails
 set -e
 
-# keep track of the last executed command
-trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
-# echo an error message before exiting
-trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
-
 # Install dependencies needed for qemu
 sudo apt-get install qemu qemu-user-static debootstrap gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 # Create rootfs/chroot for arm64
