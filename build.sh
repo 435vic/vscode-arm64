a6fbae055ed3e9402bed783fa31e3f20fdf012c6
Victor Javier <victor_javier2004@hotmail.com>
2018-10-28 05:08:52 +0000

    Add extra yarn commands Saw the build scripts from headmelted to build vscode, so trying some stuff done there.

--- build.sh
+++ build.sh
@@ -6,5 +6,10 @@ cd vscode
 export npm_config_arch=arm64
 
 yarn
+yarn --verbose gulp electron-arm64
+yarn --verbose monaco-compile-check
+yarn --verbose strict-null-check
+yarn --verbose compile
+yarn --verbose download-builtin-extensions
 yarn run gulp vscode-linux-arm64-min
 yarn run gulp vscode-linux-arm64-build-deb
