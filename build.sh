6a6d481236ccd15e63da175cd028b78558663d68
Victor Javier <victor_javier2004@hotmail.com>
2018-10-28 05:20:25 +0000

    Remove strict-null-check Yarn said command not found

--- build.sh
+++ build.sh
@@ -8,7 +8,6 @@ export npm_config_arch=arm64
 yarn
 yarn --verbose gulp electron-arm64
 yarn --verbose monaco-compile-check
-yarn --verbose strict-null-check
 yarn --verbose compile
 yarn --verbose download-builtin-extensions
 yarn run gulp vscode-linux-arm64-min
