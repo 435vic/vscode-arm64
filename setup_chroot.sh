b0111ed5ecc9c30906c8277621a9610198bd7192
Victor Javier <victor_javier2004@hotmail.com>
2018-11-01 12:43:50 -0600

    Install libx11 on rootfs

--- setup_chroot.sh
+++ setup_chroot.sh
@@ -1,2 +1,5 @@
 sudo apt-get install qemu qemu-user-static debootstrap gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 sudo qemu-debootstrap --arch=arm64 --variant=minbase xenial rootfs
+sudo apt-get update
+sudo apt-get upgrade
+sudo chroot rootfs apt-get install -y libx11-dev
