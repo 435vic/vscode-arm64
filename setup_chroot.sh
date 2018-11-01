41e6c84303b8e6316acece9238a4ce78c9424461
Victor Javier <victor_javier2004@hotmail.com>
2018-11-01 13:08:50 -0600

    Update before libx11 install

--- setup_chroot.sh
+++ setup_chroot.sh
@@ -1,5 +1,5 @@
 sudo apt-get install qemu qemu-user-static debootstrap gcc-arm-linux-gnueabihf g++-arm-linux-gnueabihf
 sudo qemu-debootstrap --arch=arm64 --variant=minbase xenial rootfs
 sudo apt-get update
-sudo apt-get upgrade
-sudo chroot rootfs apt-get install -y libX11-dev
+sudo chroot rootfs apt-get update
+sudo chroot rootfs apt-get install -y lib11-dev
