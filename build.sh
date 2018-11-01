0c9b479618f2eaef4ff876766843a4750e810872
Victor Javier <victor_javier2004@hotmail.com>
2018-11-01 07:55:14 -0600

    Change arm64 to aarch64

--- build.sh
+++ build.sh
@@ -3,18 +3,18 @@ set -e
 
 cd vscode
 
-export npm_config_arch=arm64
+export npm_config_arch=aarch64
 
 yarn
-yarn --verbose gulp electron-arm64
+yarn --verbose gulp electron-aarch64
 yarn --verbose monaco-compile-check
 yarn --verbose compile
 yarn --verbose download-builtin-extensions
-yarn run gulp vscode-linux-arm64-min
-yarn run gulp vscode-linux-arm64-build-deb
+yarn run gulp vscode-linux-aarch64-min
+yarn run gulp vscode-linux-aarch64-build-deb
 
 # Zip
 ls -la
-cd ../VSCode-linux-arm64
-tar czf ../VSCode-linux-arm64-${LATEST_MS_TAG}.tar.gz .
+cd ../VSCode-linux-aarch64
+tar czf ../VSCode-linux-aarch64-${LATEST_MS_TAG}.tar.gz .
 
